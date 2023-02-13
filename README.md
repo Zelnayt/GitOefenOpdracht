bb6398435ab765a8818ddd782161b6f55935313e
LuukBosmann <Luuk.Bosmann@student.graafschapcollege.nl>
2023-02-13 08:57:56 +0100

    First changes

---
+++ README.md
@@ -0,0 +1,38 @@
+# Oefenproject Git Deployment
+Dit is de oefenopdracht:
+ 
+1. Clone dit project naar je locale machine
+   
+2. Open met VSCode of andere IDE
+
+3. verwijder de .git directory
+
+Powershell (PS):
+
+```
+Remove-Item '.git' -Recurse
+```
+
+CMD: 
+    
+```
+rmdir .git
+```
+
+unix: 
+    
+```
+rm -rf .git
+```
+
+4. maak een nieuwe lokale repository
+    
+5. Pas het project aan en maak een aantal commits:
+    - Zet er een ander plaatje in!
+    - Maak van index.html index.php
+    - Zorg dat de actuele datum en tijd op de pagina komt te staan
+    - Zet je naam op de pagina
+    
+6. Zorg dat dit project via een private github repository naar je linux machine wordt gedeployed.
+    
+7. Laat de docent de website zien draaien vanaf je lokale unix machine.
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Git Oefen Opdracht</title>
+</head>
+<body>
+<div class="container">
+
+    <div class="jumbotron">
+    <h1>Git Oefenopdracht</h1><br>
+   
+    <h2>
+        <?php
+            // date and time now
+            $date = date('d-m-Y H:i:s');
+            echo $date;
+        ?>
+    </h2><br>
+
+    <p>
+        Luuk Bosmann
+    </p>
+
+    </div>
+    <img src="img/feelsbadman-memes-pepe-meme-wallpaper-preview.jpg" class="img-rounded" alt="Git Deployment">
+</div>
+</body>
+</html>
